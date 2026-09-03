d07b0e57dbbd9c455abf22081be7eb350ffd583e
freexiaochuan <w2239559319@myyahoo.com>
2026-09-03 11:59:23 +0800

    u

--- src/train.py
+++ src/train.py
@@ -7,8 +7,8 @@ from torchvision import transforms
 from tqdm import tqdm
 from huggingface_hub import hf_hub_download
 
-from model import CaptchaViT
-from dataset.dataset import CaptchaDataset, ctc_collate_fn
+from .model import CaptchaViT
+from .dataset.dataset import CaptchaDataset, ctc_collate_fn
 
 def download_pt():
 
