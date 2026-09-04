96e5ca26d42d82c9fa1f67fb034d073d3c809070
freexiaochuan <w2239559319@myyahoo.com>
2026-09-04 16:55:17 +0800

    u

--- src/dataset/save.py
+++ src/dataset/save.py
@@ -10,10 +10,10 @@ def resize_keep_ratio(img, target_w=128, target_h=32):
     padding = (delta_w // 2, delta_h // 2, delta_w - delta_w // 2, delta_h - delta_h // 2)
     return ImageOps.expand(img, padding, fill=255)
 
-with open('./data.json', 'r', encoding='utf-8') as f:
-    data = json.load(f)
-
 def save():
+
+    with open('./data.json', 'r', encoding='utf-8') as f:
+        data = json.load(f)
     img_list = []
     label_list = []
 
@@ -37,5 +37,26 @@ def save():
     torch.save({'images': all_images, 'labels': label_list}, './captcha_dataset.pt')
     print(f"保存完成！总共 {len(img_list)} 张，内存占用 {all_images.nbytes / 1024 / 1024:.2f} MB")
 
+def save_predict():
+    with open('./model_predict_data.json', 'r', encoding='utf-8') as f:
+        data = json.load(f)
+
+    img_list = []
+    label_list = []
+
+    count = 0
+    for key, value in data.items():
+        if count > 3700:
+            break
+        img = Image.open(key).convert('L')
+        img = resize_keep_ratio(img)
+        img_array = np.array(img)
+        img_list.append(img_array)
+        label_list.append(value)
+        count += 1
+    all_images = np.stack(img_list, axis=0)
+    torch.save({'images': all_images, 'labels': label_list}, './new_dataset.pt')
+    print(f"保存完成！总共 {len(img_list)} 张，内存占用 {all_images.nbytes / 1024 / 1024:.2f} MB")
+
 if __name__ == '__main__':
-    save()
+    save_predict()
